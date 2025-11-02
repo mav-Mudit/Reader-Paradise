57f2825e527b90f505e83df857e792157df38783
Mudit <mudittandon34@gmail.com>
2025-11-02 17:56:54 +0100

    Banner and Footer added

--- frontend/bookstore_frontend/src/App.jsx
+++ frontend/bookstore_frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
+      <Footer></Footer>
     </>
   );
 }
