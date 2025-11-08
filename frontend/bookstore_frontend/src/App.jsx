c2f37e3ed49bf71d93a5efe0d15a7b32c9ffdc34
Mudit <mudittandon34@gmail.com>
2025-11-08 18:38:29 +0100

    Home Page Done!

--- frontend/bookstore_frontend/src/App.jsx
+++ frontend/bookstore_frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import FreeBook from "./components/FreeBook";
 
 export default function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <FreeBook></FreeBook>
       <Footer></Footer>
     </>
   );
